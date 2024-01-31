a3697c58c6f6afdf7bef55946bc963826a63691a
Asthestarsfalll <1186454801@qq.com>
2024-01-31 14:51:14 +0800

    update for windows

--- plugins/alpha.lua
+++ plugins/alpha.lua
@@ -90,33 +90,36 @@ return {
     local button = require("astronvim.utils").alpha_button
     local get_icon = require("astronvim.utils").get_icon
 
-    -- dashboard.section.header.val = WEEKDAYS[getDayOfWeek()]
+    if vim.loop.os_uname().sysname == "Linux" then
+      dashboard.opts.opts.noautocmd = true
+      dashboard.section.terminal.opts.redraw = true
+      local idx = math.random(1, #ANIME)
+      local info = ANIME[idx]
+      path = os.getenv "HOME" .. "/.config/nvim/lua/user/plugins/"
+      
+      if idx > 4 then
+        dashboard.section.terminal.command = "sh " .. path .. "show.sh " .. path .. info[1]
+      else
+        dashboard.section.terminal.command = "cat " .. path .. info[1]
+      end
+      dashboard.section.terminal.width = info[2]
+      dashboard.section.terminal.height = info[3]
 
-    dashboard.opts.opts.noautocmd = true
-    dashboard.section.terminal.opts.redraw = true
-    local idx = math.random(1, #ANIME)
-    local info = ANIME[idx]
-    path = os.getenv "HOME" .. "/.config/nvim/lua/user/plugins/"
-    if idx > 4 then
-      dashboard.section.terminal.command = "sh " .. path .. "show.sh " .. path .. info[1]
-    else
-      dashboard.section.terminal.command = "cat " .. path .. info[1]
-    end
-    dashboard.section.terminal.width = info[2]
-    dashboard.section.terminal.height = info[3]
+      if idx == 4 then
+        dashboard.section.buttons.val = {
+          button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+        }
+      end
 
-    if idx == 4 then
-      dashboard.section.buttons.val = {
-        button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+      dashboard.opts.layout = {
+        dashboard.section.terminal,
+        { type = "padding", val = 2 },
+        dashboard.section.buttons,
+        dashboard.section.footer,
       }
+    else
+      dashboard.section.header.val = WEEKDAYS[getDayOfWeek()]
     end
-
-    dashboard.opts.layout = {
-      dashboard.section.terminal,
-      { type = "padding", val = 2 },
-      dashboard.section.buttons,
-      dashboard.section.footer,
-    }
     return dashboard
   end,
   config = function(_, opts)
